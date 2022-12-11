960a136459915886868fd1b3e29e3fe4cc76fc64
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2022-12-11 17:13:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//console.log("Welcome to Node API Tutorial");
+
+const express= require("express");
+const bodyParser= require("body-parser");
+
+const app= express();
+
+const port= 5000;
+
+const Urlpth= require("./router");
+
+app.use(bodyParser.json());
+
+app.use("/", Urlpth);
+
+app.listen(port, ()=>console.log("Server running on port 5000"));
