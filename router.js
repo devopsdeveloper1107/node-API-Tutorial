99616f4b4ea69833dd9428f1de4e9756632dc403
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2022-12-22 07:24:49 +0530

    Update router.js

--- router.js
+++ router.js
@@ -1,10 +1,67 @@
 const express= require("express");
-
 const Router= express.Router();
 
+const dbconnected= require("./dbconnection");
+
 Router.get("/",(req, res)=>{
-    const data=[{name:"john USA",email:"john007@gmail.com"}];
+    const data=[{name:"David Test",email:"john007@gmail.com"}];
     res.send(data);
 });
+
+Router.get("/api/user",(req, res)=>{
+    dbconnected.query("select * from tbl_user", (err, rows, fields)=>{
+        if(!err)
+        {
+        res.send(rows);
+        } else {
+            console.log(err);
+        }
+    })
+})
+
+Router.get("/api/country", (req, res)=>{
+    dbconnected.query("select * from tbl_country",(err, rows)=>{
+        if(!err){
+        res.send(rows);
+        } else{
+            console.log(err);
+        }
+    });
+
+});
+
+Router.get("/api/state/:id",(req, res)=>{
+    dbconnected.query("select * from tbl_state where countryid='"+req.params.id +"'   ", (err, rows)=>{
+     
+        if(!err)
+        {
+        res.send(rows);
+        } else{
+            console.log(err);
+        }
+    });
+
+});
+
+
+Router.get("/api/countrystate",(req, res)=>{
+    var sql = "SELECT * FROM tbl_country";
+    dbconnected.query(sql, (err, rows)=>{
+     
+        if(!err)
+        {
+        res.send(rows);
+        } else{
+            console.log(err);
+        }
+    });
+
+});
+
+
+
+
+
+
 
 module.exports= Router;
