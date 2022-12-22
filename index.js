65c96bf1c623b30a483dd5dbe26e04aaf42e0a9b
Devops Developer <100074939+devopsdeveloper1107@users.noreply.github.com>
2022-12-22 07:25:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-///console.log("Welcome to Node API Tutorial");
+//console.log("Welcome to Node API Tutorial");
+// npm install --save-dev nodemon
+// npm install cors
 
 const express= require("express");
 const bodyParser= require("body-parser");
+const cors= require("cors");
 
 const app= express();
 
@@ -10,7 +13,10 @@ const port= 5000;
 const Urlpth= require("./router");
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", Urlpth);
+app.use("/api/country", Urlpth);
+
 
 app.listen(port, ()=>console.log("Server running on port 5000"));
